cb13665b1b9ebf8f2202a06a5730833c0b584074
Faisal N <narsi.faisal@gmail.com>
2017-04-16 22:44:20 -0500

    Delete keys.js

--- controller/keys.js
+++
@@ -1,8 +0,0 @@
-var keys = {
-   testSecretKey : "sk_test_9fG6JsdlSSYHBtWH7kTZTRSW",
-   testPublishableKey : "pk_test_zrudxgkNhgayqBV74LjXMoCZ ",
-   liveSecretKey: "sk_live_aFwLsVTrK4wIXFNKjHgpsy88 ",
-   livePublishableKey: "pk_live_ujgerB3Xb2GsTgoHJaGQdrVE "
-}
-
-module.exports = keys;
